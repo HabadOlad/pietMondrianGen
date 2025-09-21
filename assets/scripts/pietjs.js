7b7e2e0b5d5b823558133f79e58757adbe0d91b5
Habad Olad <olad.hab@gmail.com>
2025-09-21 18:27:45 +0100

    feat:brand new css

--- assets/scripts/pietjs.js
+++ assets/scripts/pietjs.js
@@ -1,4 +1,7 @@
-const canvas = document.getElementById("myCanvas");
-const ctx = canvas.getContext("2d");
+let canvas = document.getElementById("myCanvas");
+let ctx = canvas.getContext("2d");
+
+let size = window.innerWidth; //this line of code gets the inner width of the browser window in pixels
+let dpr = window.devicePixelRatio;
 
 //Generate(){}
